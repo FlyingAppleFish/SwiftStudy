d8670572ffb8583d3a4d5f31f9967e98e55b8bb9
wangyongfei <wangyongfei666@gmail.com>
2016-06-14 10:40:44 +0800

    Swift项目开始搭建

---
+++ TDClient_Swift/TDClient_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDClient_Swift
+//
+//  Created by WYF_Mac on 16/6/14.
+//  Copyright © 2016年 WYF_Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
